11e5a8ac18d78975ddf5b2118c1ee37e30310138
dweb01 <webd868@gmail.com>
2024-08-19 16:59:04 +0700

    udpdate from offfice

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    public static BulletScript Instance;
+    // public GameObject netPrefab;
+    [SerializeField] float netTime = 0.3f;
+
+    [SerializeField] float damage = 1f;
+
+    private ShootingScript shootingScript;
+
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start() {
+        shootingScript = ShootingScript.Instance;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Fish"))
+        {
+             SpriteRenderer fishSpriteRenderer = other.GetComponent<SpriteRenderer>();
+             FishScript getfishScript = other.GetComponent<FishScript>();
+
+            if (fishSpriteRenderer != null)
+            {
+                Color fishColor = fishSpriteRenderer.color;
+                Color colorFishBackup = fishColor;
+                fishColor.a = 0.70f;
+                fishSpriteRenderer.color = fishColor;
+
+                getfishScript.ResetFishColor(fishSpriteRenderer,colorFishBackup);
+                getfishScript.TakeDamage(fishSpriteRenderer, damage);
+                
+            }
+            
+            float fishCenterY = other.bounds.center.y;
+
+            Vector3 netPosition = new Vector3(transform.position.x, fishCenterY, transform.position.z);
+
+
+            Animator netInstance = Instantiate(shootingScript.Net, netPosition, Quaternion.identity);
+
+            Destroy(netInstance.gameObject, netTime);
+
+    
+            Destroy(gameObject);
+        }
+    }
+
+
+    void OnBecameInvisible()//Out of screen phone
+    {
+        
+    }
+
+
+
+}
---
+++ Assets/Scripts/FishScript.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishScript : MonoBehaviour
+{   
+    [SerializeField] public float Hp = 5f;
+
+    // [SerializeField] public int id;
+
+
+    private BulletScript bulletScript;
+
+
+
+    void Awake()
+    {
+        
+    }
+
+
+     void Start()
+    {
+        bulletScript = BulletScript.Instance;
+    }
+
+    private bool IsDead(){
+        return Hp <= 0;
+    }
+
+
+
+
+
+    public void ResetFishColor(SpriteRenderer Fish, Color backUpColor)
+    {
+        StartCoroutine(IEnumResetFishColor(Fish, backUpColor));
+    }
+
+    private IEnumerator IEnumResetFishColor(SpriteRenderer Fish, Color backUpColor)
+    {
+        yield return new WaitForSeconds(0.1f);
+        Fish.color = backUpColor;
+    }
+
+
+
+    public void TakeDamage(SpriteRenderer Fish, float Damage){
+        Hp -= Damage;      
+        FishSystem(Fish);
+    }
+
+    private void FishSystem(SpriteRenderer Fish){
+        if(IsDead()){
+            Fish.gameObject.SetActive(false);
+        }
+    }   
+
+
+}
